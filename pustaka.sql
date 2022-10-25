33e0c04d09f9b1c6d3b338d54512fb3601018b82
Veteradrianus <peterardianus@gmail.com>
2022-10-25 15:46:43 +0700

    database tugas 5

--- pustaka.sql
+++ pustaka.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 17 Okt 2022 pada 11.04
--- Versi server: 10.4.24-MariaDB
--- Versi PHP: 8.1.6
+-- Generation Time: 25 Okt 2022 pada 10.42
+-- Versi Server: 10.1.30-MariaDB
+-- PHP Version: 5.6.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `perpus`
+-- Database: `pustaka`
 --
 
 -- --------------------------------------------------------
@@ -29,33 +30,33 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `buku` (
   `id` int(11) NOT NULL,
-  `judul_buku` varchar(128) CHARACTER SET latin1 NOT NULL,
+  `judul_buku` varchar(128) NOT NULL,
   `id_kategori` int(11) NOT NULL,
-  `pengarang` varchar(64) CHARACTER SET latin1 NOT NULL,
-  `penerbit` varchar(64) CHARACTER SET latin1 NOT NULL,
+  `pengarang` varchar(64) NOT NULL,
+  `penerbit` varchar(64) NOT NULL,
   `tahun_terbit` year(4) NOT NULL,
-  `isbn` varchar(64) CHARACTER SET latin1 NOT NULL,
+  `isbn` varchar(64) NOT NULL,
   `stok` int(11) NOT NULL,
   `dipinjam` int(11) NOT NULL,
-  `diboking` int(11) NOT NULL,
-  `image` varchar(256) CHARACTER SET latin1 NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `dibooking` int(11) NOT NULL,
+  `image` varchar(256) DEFAULT 'book-default-cover.jpg'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `buku`
 --
 
-INSERT INTO `buku` (`id`, `judul_buku`, `id_kategori`, `pengarang`, `penerbit`, `tahun_terbit`, `isbn`, `stok`, `dipinjam`, `diboking`, `image`) VALUES
-(1, 'Statiska dengan Program Komputer', 1, 'Ahmad Kholiqul Amin', 'Deep Publish', 2014, '9786022809432', 6, 1, 1, 'img1557402455.jpg'),
-(2, 'Mudah Belajar Komputer untuk anak', 1, 'Bambang Agus Setiawan', 'Hutan Media', 2014, '9786025118500', 5, 3, 1, 'img1555522493.jpg'),
-(5, 'PHP Komplet ', 1, 'Jubilee', 'Elex Media Komputindo', 2017, '834653547', 5, 1, 1, 'img1555522493.jpg'),
-(10, 'Detektif Conan Ep 200', 9, 'Okigawa sasuke', 'Cultura', 2016, '874387583987', 5, 0, 0, 'img1557401465.jpg'),
-(14, 'Bahasa Indonesia', 2, 'Umri Nur\'aini dan indriyani', 'Pusat Perbukuan', 2015, '757254724884', 3, 0, 0, 'img1557402703.jpg'),
-(15, 'Komunukasi\r\nLantas \r\nBudaya', 5, 'Dr. Dady Kurnia', 'Published', 2015, '878674646488', 5, 0, 0, 'img1557403156.jpg'),
-(16, 'Kolaborasi\r\nCodeigniter\r\ndan Ajax\r\ndalam\r\nPercabangan', 1, 'Anton Subagia', 'Elex Media Komputindo', 2017, '43345356577', 5, 0, 0, 'img1557403658.jpg'),
-(17, 'From Hobby to Money', 4, 'Deasylawati', 'Elex Media Komputindo', 2015, '87968686787879', 5, 0, 0, 'img1557403658.jpg'),
-(18, 'Buku saku Pramuka', 8, 'Rudi Himawan', 'Pusat Perbukuan', 2016, '97868687978796', 6, 0, 0, 'img1557404613.jpg'),
-(19, 'Rahasia Keajaiban Bumi', 3, 'Nurul Ihsan ', 'Luxima', 2014, '565756565768868', 5, 0, 0, 'img155740468.jpg');
+INSERT INTO `buku` (`id`, `judul_buku`, `id_kategori`, `pengarang`, `penerbit`, `tahun_terbit`, `isbn`, `stok`, `dipinjam`, `dibooking`, `image`) VALUES
+(1, 'statistika\r\ndengan\r\nprogram\r\nkomputer', 1, 'Ahmad\r\nKholiqul Amin', 'Deep\r\npublish', 2014, '9786022809432', 6, 1, 1, 'img1557402455.jpg'),
+(2, 'Mudah\r\nBelajar\r\nKomputer\r\nuntuk Anak', 1, 'Bambang\r\nAgus\r\nSetiawan', 'Huta Media', 2014, '9786025118500', 5, 3, 1, 'img1557402397.jpg'),
+(5, 'PHP Komplet', 1, 'Jubilee', 'Elex Media\r\nKomputindo', 2017, '8346753547', 5, 1, 1, 'img1555522493.jpg'),
+(10, 'Detektif \r\nConan Ep\r\n200', 9, 'Okigawa\r\nSasuke', 'Cultura', 2016, '874387583987', 5, 0, 0, 'img1557401465.jpg'),
+(14, 'Bahasa\r\nIndonesia', 2, 'Umri Nur\'aini\r\nDan Indriyani', 'Pusat\r\nPerbukuan', 2015, '757254724884', 3, 0, 0, 'img1557402703.jpg'),
+(15, 'Komunikasi \r\nLintas\r\nBudaya', 5, 'Dr.Dedy\r\nKurnia', 'published', 2016, '878674646488', 5, 0, 0, 'img1557403156.jpg'),
+(16, 'Kolaborasi\r\nCodeigniter\r\ndan Ajax\r\ndalam\r\nPerancangan', 1, 'Anton\r\nSubagia', 'Elex Media\r\nKomputindo', 2017, '43345356577', 5, 0, 0, 'img1557403502.jpg'),
+(17, 'From Hobby\r\nto Money', 4, 'Deasylawati', 'Elex Media\r\nKomputindo', 2015, '87968686787879', 5, 0, 0, 'img1557403658.jpg'),
+(18, 'Buku Saku\r\nPramuka', 8, 'Rudi\r\nHimawan', 'Pusat\r\nPerbukuan', 2016, '97868687978796', 6, 0, 0, 'img1557404613.jpg'),
+(19, 'Rahasia\r\nKeajaiban\r\nBumi', 3, 'Nurul Ihsan', 'Luxima', 2014, '565756565768868', 5, 0, 0, 'img1557404689.jpg');
 
 -- --------------------------------------------------------
 
@@ -64,15 +65,15 @@ INSERT INTO `buku` (`id`, `judul_buku`, `id_kategori`, `pengarang`, `penerbit`, 
 --
 
 CREATE TABLE `kategori` (
-  `id_karegori` int(5) NOT NULL,
-  `nama_kategori` varchar(45) CHARACTER SET latin1 NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `id_kategori` int(5) NOT NULL,
+  `nama_kategori` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `kategori`
 --
 
-INSERT INTO `kategori` (`id_karegori`, `nama_kategori`) VALUES
+INSERT INTO `kategori` (`id_kategori`, `nama_kategori`) VALUES
 (1, 'Komputer'),
 (2, 'Bahasa'),
 (3, 'Sains'),
@@ -91,15 +92,15 @@ INSERT INTO `kategori` (`id_karegori`, `nama_kategori`) VALUES
 
 CREATE TABLE `role` (
   `id` int(11) NOT NULL,
-  `role` varchar(128) CHARACTER SET latin1 NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `role` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `role`
 --
 
 INSERT INTO `role` (`id`, `role`) VALUES
-(1, 'administrator'),
+(1, 'admministrator'),
 (2, 'member');
 
 -- --------------------------------------------------------
@@ -110,43 +111,67 @@ INSERT INTO `role` (`id`, `role`) VALUES
 
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
-  `nama` varchar(128) CHARACTER SET latin1 NOT NULL,
-  `email` varchar(128) CHARACTER SET latin1 NOT NULL,
-  `image` varchar(128) CHARACTER SET latin1 NOT NULL,
-  `password` varchar(256) CHARACTER SET latin1 NOT NULL,
+  `nama` varchar(128) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `image` varchar(128) NOT NULL,
+  `password` varchar(256) NOT NULL,
   `role_id` int(11) NOT NULL,
   `is_active` int(1) NOT NULL,
   `tanggal_input` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `role`
+-- Indexes for table `buku`
+--
+ALTER TABLE `buku`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indexes for table `role`
 --
 ALTER TABLE `role`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `user`
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `role`
+-- AUTO_INCREMENT for table `buku`
+--
+ALTER TABLE `buku`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id_kategori` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `role`
 --
 ALTER TABLE `role`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT untuk tabel `user`
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
